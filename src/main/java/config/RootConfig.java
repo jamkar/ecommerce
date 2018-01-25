c6c6750bdb66a314781d169ad1f7a2100f456bbb
jamkar <karenjamalyan@gmail.com>
2018-01-25 11:21:23 +0400

    added property source

--- src/main/java/config/RootConfig.java
+++ src/main/java/config/RootConfig.java
@@ -2,10 +2,9 @@ package config;
 
 import org.apache.commons.dbcp2.BasicDataSource;
 import org.hibernate.SessionFactory;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.ImportResource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.*;
+import org.springframework.core.env.Environment;
 import org.springframework.orm.hibernate5.HibernateTransactionManager;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -18,15 +17,19 @@ import java.util.Properties;
 @ComponentScan(basePackages = {"service", "dao"})
 @EnableTransactionManagement
 @ImportResource("classpath:spring-jms.xml")
+@PropertySource(value = {"classpath:application.properties"})
 public class RootConfig {
+
+    @Autowired
+    Environment env;
 
     @Bean
     public DataSource dataSource() {
         BasicDataSource dataSource = new BasicDataSource();
         dataSource.setDriverClassName("com.mysql.jdbc.Driver");
-        dataSource.setUrl("jdbc:mysql://localhost:3306/ecommerce");
-        dataSource.setUsername("root");
-        dataSource.setPassword("root");
+        dataSource.setUrl(env.getProperty("db.url"));
+        dataSource.setUsername(env.getProperty("db.username"));
+        dataSource.setPassword(env.getProperty("db.password"));
         dataSource.setInitialSize(2);
         dataSource.setMaxTotal(5);
         return dataSource;
